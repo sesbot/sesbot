3b7763b2fb2d62ca2a7acf9d3bc7119d8ebdc7c8
Statewide Emergency Services <progamerzbeastmode@gmail.com>
2019-10-26 21:43:01 -0500

    Update rules.js

--- commands/rules.js
+++ commands/rules.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 
 module.exports.run = async(bot, message, args) => {
-    const WebsiteEmbed = CreateEmbed(`Hey, **${message.author.username}** 👋\n\nHere is all the  **[Rules & Regulations](Under Development)**\n\nPlease Follow **ALL** Statewide Emergency Services Rules. These rules are in place for a reason to help keep everyone happy and to keep up the fun! Any questions please contact ${bot.user.toString()}\n\nThank you`, `**Statewide Emergency Services Rules**`,`From Statewide Emergency Services Admin Team.`)
+    const WebsiteEmbed = CreateEmbed(`Hey, **${message.author.username}** 👋\n\nHere is all the  **[Rules & Regulations](#discord-information)**\n\nPlease Follow **ALL** Statewide Emergency Services Rules. These rules are in place for a reason to help keep everyone happy and to keep up the fun! Any questions please contact ${bot.user.toString()}\n\nThank you`, `**Statewide Emergency Services Rules**`,`From Statewide Emergency Services Admin Team.`)
     message.channel.send(WebsiteEmbed);
     message.delete(1000);
 }
@@ -13,7 +13,7 @@ module.exports.help = {
 
 function CreateEmbed(Description, author, footer)
 {
-    const EmbedColor = 0x80bfff;
+    const EmbedColor = 0x211e56;
     const newChannel = new Discord.RichEmbed()
     .setColor(EmbedColor)
     .setDescription(Description)
